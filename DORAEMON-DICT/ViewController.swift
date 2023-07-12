fee554767272ff1fc599b7cd3524faa21a0e66ab
Fraser Chang <shuhan0628@gmail.com>
2023-07-12 10:27:14 +0800

    Initial Commit

---
+++ DORAEMON-DICT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DORAEMON-DICT
+//
+//  Created by 張丰澤 on 2023/7/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
